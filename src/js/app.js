13bca6372e0a27b2e6160d5bf217d889a38c172c
Victor Shurko <victorshurko@yandex.ru>
2024-12-10 09:26:06 +0300

    updating function from map

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,7 @@
 export default function specialAttack({ special }) {
-    const result = [];
-    for(const attack of special) {
-        const { id, name, description = 'Описание недоступно', icon } = attack;
-        result.push({
-            id,
-            name,
-            description,
-            icon
-        });
-    }
-
-    return result;
-}
+    return special.map(({ id, name, description = 'Описание недоступно', icon }) => (
+        {
+            id, name, description, icon
+        }
+    ));
+};
